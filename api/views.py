2245c5e0c420b0f4dfe620f662e885eb67fd1814
Zubastic120993 <vladymyrzub@gmail.com>
2025-05-16 08:26:42 +0200

    Add views and custom permissions

--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .models import Category, Task
+from .serializers import CategorySerializer, TaskSerializer
+from .permissions import IsOwner
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
+
+    def get_queryset(self):
+        """
+        This view should return a list of all categories
+        for the currently authenticated user.
+        """
+        return self.queryset.filter(owner=self.request.user)
+    
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [ 'category','owner']
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+    
+    
