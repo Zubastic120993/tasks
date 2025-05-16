c80e1f07743831d9ea9140645d272264c88e9821
Zubastic120993 <vladymyrzub@gmail.com>
2025-05-16 08:35:54 +0200

    Add URL routing for API and documentation

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api-token-auth/", views.obtain_auth_token, name="token"),
+    path("api-auth/", include("rest_framework.urls")),
 ]
