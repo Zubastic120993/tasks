d59ec9e64ebda65c65f697590f6c29dc4cc5ecc5
Zubastic120993 <vladymyrzub@gmail.com>
2025-05-16 10:33:41 +0200

    Register Category,Task and User in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Task, Category
+from django.contrib.auth.models import User
 
 # Register your models here.
+admin.site.unregister(User)
+ 
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority', 'completed', 'created_at', 'due_date')
+    list_filter = ('priority', 'completed')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 10
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active')
+    search_fields = ('username', 'email')
+    list_per_page = 10
+
